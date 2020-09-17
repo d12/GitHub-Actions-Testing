031f98cc506212fc11b7d94cf3f5e2b93bd68bdb
Nathaniel Woodthorpe <nathaniel.woodthorpe@shopify.com>
2020-09-17 17:46:34 -0400

    Update

--- .github/scripts/card_adder/card_adder.rb
+++ .github/scripts/card_adder/card_adder.rb
@@ -1,8 +1,11 @@
 require "slop"
 
 opts = Slop.parse do |o|
-  o.string "--label_name", "The name of the label used to add issue to project board"
+  o.string "--label_name", "The name of the label we're being notified about"
   o.string "--action", "The label action taken. One of 'labeled' or 'unlabeled'"
+  o.string "--issue_title", "The title of the issue"
+  o.string "--issue_url", "The URL of the issue"
+  o.string "--issue_number", "The number of the issue"
 end
 
 puts opts.to_h
