e25529eeb5b260db707504971f097c74744a508c
Nathaniel Woodthorpe <nathaniel.woodthorpe@shopify.com>
2020-09-18 11:03:56 -0400

    Prettyify the Ruby scripts

--- .github/scripts/card_adder.rb
+++ .github/scripts/card_adder.rb
@@ -6,42 +6,65 @@ GITHUB_PROJECT_TODO_COLUMN_ID = 10860613
 GITHUB_PROJECT_BOARD_URL = "https://github.com/d12/GitHub-Actions-Testing/projects/1"
 GITHUB_REPO_NWO = "d12/GitHub-Actions-Testing"
 
-opts = Slop.parse do |o|
-  o.string "--label_name", "The name of the label we're being notified about"
-  o.string "--action", "The label action taken. One of 'labeled' or 'unlabeled'"
-  o.string "--issue_title", "The title of the issue"
-  o.string "--issue_url", "The URL of the issue"
-  o.string "--issue_number", "The number of the issue"
-  o.string "--github_token", "The GitHub token"
+def args
+  @args ||= Slop.parse do |o|
+    o.string "--label_name", "The name of the label we're being notified about"
+    o.string "--action", "The label action taken. One of 'labeled' or 'unlabeled'"
+    o.string "--issue_title", "The title of the issue"
+    o.string "--issue_url", "The URL of the issue"
+    o.string "--issue_number", "The number of the issue"
+    o.string "--github_token", "The GitHub token"
+  end
 end
 
-puts "Options: #{opts.to_h}"
+def github_client
+  @github_client ||= Octokit::Client.new(access_token: args[:github_token])
+end
 
-client = Octokit::Client.new(access_token: opts[:github_token])
+# Is this webhook event firing for the magic label?
+def event_for_magic_label?
+  args[:label_name] == MAGIC_LABEL_NAME
+end
 
-unless opts[:label_name] == MAGIC_LABEL_NAME
+def issue_labeled?
+  args[:action] == "labeled"
+end
+
+def add_card_to_board!
+  github_client.create_project_card(GITHUB_PROJECT_TODO_COLUMN_ID,
+    note: "(**From Borg**) [#{args[:issue_title]}](#{args[:issue_url]})"
+  )
+end
+
+def add_comment_to_issue!(card_id)
+  github_client.add_comment(GITHUB_REPO_NWO,
+    args[:issue_number],
+    "Beep boop. Because this was labeled with #{MAGIC_LABEL_NAME}, \
+     I've added it to [the shared project board](#{GITHUB_PROJECT_BOARD_URL}).\
+     \n\n<sub>Card ID: #{card_id}</sub>"
+  )
+end
+
+# Script begin
+
+puts "Arguments: #{args.to_h}"
+
+unless event_for_magic_label?
   puts "Label is not interesting. Bailing!"
-  exit
+  exit 0
 end
 
-unless opts[:action] == "labeled"
+unless issue_labeled?
   puts "Label was removed, not added. Bailing!"
-  exit
+  exit 0
 end
 
 puts "Adding note to project board..."
 
-created_card = client.create_project_card(GITHUB_PROJECT_TODO_COLUMN_ID,
-  note: "(**From Borg**) [#{opts[:issue_title]}](#{opts[:issue_url]})"
-)
+created_card = add_card_to_board!
 
 puts "Adding comment to issue..."
 
-client.add_comment(GITHUB_REPO_NWO,
-  opts[:issue_number],
-  "Beep boop. Because this was labeled with #{MAGIC_LABEL_NAME}, \
-   I've added it to [the shared project board](#{GITHUB_PROJECT_BOARD_URL}).\
-   \n\n<sub>Card ID: #{created_card.id}</sub>"
-)
+add_comment_to_issue!(created_card.id)
 
 puts "Done!"
--- .github/scripts/move_to_todo.rb
+++ .github/scripts/move_to_todo.rb
@@ -6,38 +6,58 @@ PROJECT_IN_PROGRESS_COLUMN_ID = 10860614
 GITHUB_PROJECT_BOARD_URL = "https://github.com/d12/GitHub-Actions-Testing/projects/1"
 GITHUB_REPO_NWO = "d12/GitHub-Actions-Testing"
 
-opts = Slop.parse do |o|
-  o.string "--action", "The label action taken. One of 'labeled' or 'unlabeled'"
-  o.string "--issue_number", "The number of the issue"
-  o.string "--github_token", "The GitHub token"
+def args
+  @args ||= Slop.parse do |o|
+    o.string "--action", "The label action taken. One of 'labeled' or 'unlabeled'"
+    o.string "--issue_number", "The number of the issue"
+    o.string "--github_token", "The GitHub token"
+  end
 end
 
-puts "Options: #{opts.to_h}"
+def github_client
+  @github_client ||= Octokit::Client.new(access_token: args[:github_token])
+end
 
-client = Octokit::Client.new(access_token: opts[:github_token])
+def issue_assigned?
+  args[:action] == "assigned"
+end
 
-unless opts[:action] == "assigned"
+def magic_label_on_issue?
+  issue = github_client.issue(GITHUB_REPO_NWO, args[:issue_number])
+  issue.labels.any? { |label| label.name == MAGIC_LABEL_NAME }
+end
+
+def find_card_id
+  comments = github_client.issue_comments(GITHUB_REPO_NWO, args[:issue_number])
+  return unless comments
+
+  matching_comment = comments.find_all { |c| /Card ID: \d+/ =~ c.body }.last
+  return unless matching_comment
+
+  /Card ID: (\d+)/.match(matching_comment.body).captures.first
+end
+
+def move_card_to_in_progress!(card_id)
+  github_client.move_project_card(card_id, "bottom", column_id: PROJECT_IN_PROGRESS_COLUMN_ID)
+end
+
+# Script begin
+
+puts "Arguments: #{args.to_h}"
+
+unless issue_assigned?
   puts "Not an assigned issue."
   exit 0
 end
 
-issue = client.issue(GITHUB_REPO_NWO, opts[:issue_number])
-if issue.labels.none? { |label| label.name == MAGIC_LABEL_NAME }
+unless magic_label_on_issue?
   puts "No magic labels on the issue."
   exit 0
 end
 
 puts "Searching for Project Card ID..."
 
-comments = client.issue_comments(GITHUB_REPO_NWO, opts[:issue_number])
-
-matching_comment = comments.find_all { |c| /Card ID: \d+/ =~ c.body }.last
-unless matching_comment
-  puts "Error: Could not find Project Card ID."
-  exit 1
-end
-
-card_id = /Card ID: (\d+)/.match(matching_comment.body).captures.first
+card_id = find_card_id
 unless card_id
   puts "Error: Could not find Project Card ID."
   exit 1
@@ -45,6 +65,6 @@ end
 
 puts "Found card ID #{card_id}, moving to in progress..."
 
-client.move_project_card(card_id, "bottom", column_id: PROJECT_IN_PROGRESS_COLUMN_ID)
+move_card_to_in_progress!(card_id)
 
 puts "Done!"
