7be3321dde83d3a7de6515d65b0ebb1b784004aa
Nathaniel Woodthorpe <nathaniel.woodthorpe@shopify.com>
2020-09-18 13:51:17 -0400

    Codify the label name

--- .github/scripts/issue_labeled.rb
+++ .github/scripts/issue_labeled.rb
@@ -42,7 +42,7 @@ end
 def add_comment_to_issue!(card_id)
   github_client.add_comment(GITHUB_REPO_NWO,
     args[:issue_number],
-    "Beep boop. I saw you labeled this issue with #{MAGIC_LABEL_NAME} \
+    "Beep boop. I saw you labeled this issue with `#{MAGIC_LABEL_NAME}` \
      so I've added it to [the shared project board](#{GITHUB_PROJECT_BOARD_URL}).\
      \n\n<sub>Card ID: #{card_id}</sub>"
   )
